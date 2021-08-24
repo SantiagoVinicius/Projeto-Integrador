b61acc8ddc6e18f646489b5a219b2825a060baa0
Vinícius Santiago <viniciussantiagosantos19@gmail.com>
2021-08-24 12:18:16 -0300

    login

--- entrada.php
+++ entrada.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Área Restrita</title>
     <link href="css/estilo.css" rel="stylesheet">
     <link href="bootstrap5/css/bootstrap.css" rel="stylesheet">
 </head>
 <body class="bg-dark">
-<div class="card border-warning mb-3 bg-dark" style="max-width: 20%; margin-left:40%; margin-top:15%; text-align:center;">
-    <form name="formulario-login" action="login.php" method="post" >
-        <label>Usuário:</label>
+<div class="card border-warning mb-3 bg-dark" style="max-width: 20%; margin-left:40%; margin-top:10%; text-align:center;">
+    <form name="formulario-login" action="login.php" method="post">
+        <label style="color:white;margin-top:20px;">Área Restrita</label>
+    <hr class="card border-warning mb-3 bg-dark" >
+        <label style="color:white;">Usuário:</label>
         <input class="btn btn-warning" name="usuario">
-        <label>Senha:</label>
+        <label style="color:white;">Senha:</label>
         <input class="btn btn-warning" type="password" name="senha">
-        <hr>
+        <hr class="card border-warning mb-3 bg-dark" >
         <button type="submit" class="btn btn-outline-warning">Acessar</button><hr>
         <?php
         if(isset($_GET['mensagem'])){
