7513a5d27aad857835b11df30eeed346644103a9
Vinícius Santiago <viniciussantiagosantos19@gmail.com>
2021-08-24 10:45:31 -0300

    agenda completa

---
+++ agenda/agenda-excluir.php
@@ -0,0 +1,16 @@
+<?php
+
+include "../includes/conexao.php";
+
+$id_agenda = $_GET['id_agenda'];
+
+$sqlExcluir = "DELETE FROM tb_agenda WHERE id = {$id_agenda}";
+
+$resultado = mysqli_query($conexao , $sqlExcluir);
+
+if($resultado){
+    header('Location:agenda-listar.php?mensagem=excluido');
+}else{
+    echo "Ocorreu algum problema";
+}
+?>
