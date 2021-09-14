b5b860b617dff70729dba67c997ae21bce9c11ad
Vinícius Santiago <viniciussantiagosantos19@gmail.com>
2021-09-14 12:29:01 -0300

    login

--- entrada.php
+++ entrada.php
@@ -8,22 +8,23 @@
     <link href="css/estilo.css" rel="stylesheet">
     <link href="bootstrap5/css/bootstrap.css" rel="stylesheet">
 </head>
-<body class="bg-dark">
-<div class="card border-warning mb-3 bg-dark" style="max-width: 20%; margin-left:40%; margin-top:10%; text-align:center;">
+<body class="container" style="text-align:center;max-width:80vh;margin-top:10%;">
+
     <form name="formulario-login" action="login.php" method="post">
-        <label style="color:white;margin-top:20px;">Área Restrita</label>
-    <hr class="card border-warning mb-3 bg-dark" >
-        <label style="color:white;">Usuário:</label>
-        <input class="btn btn-warning" name="usuario">
-        <label style="color:white;">Senha:</label>
-        <input class="btn btn-warning" type="password" name="senha">
-        <hr class="card border-warning mb-3 bg-dark" >
-        <button type="submit" class="btn btn-outline-warning">Acessar</button><hr>
+        <div style="background-image:url('logi.jpg'); background-repeat:no-repeat;background-size:30em;">
+        <label style="color:white;margin-top:20px;"class="text-success">Área Restrita</label>
+    <hr class="card border-success mb-3 bg-dark" >
+        <label style="color:white;padding:1px;"class="text-success">Usuário:</label>
+        <input class="btn btn-success" name="usuario">
+        <br>
+        <label style="color:white;margin:5px;padding:2px;"class="text-success">Senha:</label>
+        <input class="btn btn-success" type="password" name="senha"><br>
+        <button type="submit" class="btn btn-outline-success"style="margin:35px;" >Acessar</button><br>
         <?php
         if(isset($_GET['mensagem'])){
             if($_GET['mensagem'] == 'errado'){
                 ?>
-                <div class="alert alert-warning">
+                <div class="alert alert-danger">
                 Usuário ou Senha inválido!
             </div>
             <?php
@@ -32,6 +33,7 @@
             }
         }
         ?>
+        </div>
     </form>
     </div>
 </body>
