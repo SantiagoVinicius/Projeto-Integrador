af1a174d40da999e691a968309953495018c116b
Vinícius Santiago <viniciussantiagosantos19@gmail.com>
2021-09-14 19:27:33 -0300

    login

--- entrada.php
+++ entrada.php
@@ -8,18 +8,18 @@
     <link href="css/estilo.css" rel="stylesheet">
     <link href="bootstrap5/css/bootstrap.css" rel="stylesheet">
 </head>
-<body class="container" style="text-align:center;max-width:80vh;margin-top:10%;">
+<body class="container" style="text-align:center;max-width:80vh;margin-top:10%;background:#ffcbdb;">
 
-    <form name="formulario-login" action="login.php" method="post">
+    <form name="formulario-login" action="login.php" method="post" style="border:1px solid #ffcbdb;box-shadow: 0 0 1em white;">
         <div style="background-image:url('logi.jpg'); background-repeat:no-repeat;background-size:30em;">
-        <label style="color:white;margin-top:20px;"class="text-success">Área Restrita</label>
-    <hr class="card border-success mb-3 bg-dark" >
-        <label style="color:white;padding:1px;"class="text-success">Usuário:</label>
-        <input class="btn btn-success" name="usuario">
+        <label style="color:white;margin-top:20px;"class="text-dark">Área Restrita</label>
+    <hr class="card border-dark mb-3 bg-dark" >
+        <label style="color:white;padding:1px;"class="text-dark">Usuário:</label>
+        <input class="btn btn-dark" name="usuario">
         <br>
-        <label style="color:white;margin:5px;padding:2px;"class="text-success">Senha:</label>
-        <input class="btn btn-success" type="password" name="senha"><br>
-        <button type="submit" class="btn btn-outline-success"style="margin:35px;" >Acessar</button><br>
+        <label style="color:white;margin:5px;padding:2px;"class="text-dark">Senha:</label>
+        <input class="btn btn-dark" type="password" name="senha"><br>
+        <button type="submit" class="btn btn-outline-dark"style="margin:35px;" >Acessar</button><br>
         <?php
         if(isset($_GET['mensagem'])){
             if($_GET['mensagem'] == 'errado'){
